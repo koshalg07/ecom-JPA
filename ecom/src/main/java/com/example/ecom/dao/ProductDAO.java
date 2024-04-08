ca00681d8a5829ea0a750d5159ee01864a936aa7
koshalg07 <koshalg0777@gmail.com>
2024-04-08 13:16:28 +0530

    Ecom api with JPA no Inject

--- ecom/src/main/java/com/example/ecom/dao/ProductDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecom.dao;
-
-import java.util.List;
-
-import com.example.ecom.model.Product;
-
-public interface ProductDAO {
-	List<Product> getAllProducts();
-}
--- ecom/src/main/java/com/example/ecom/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecom.service;
-
-import java.util.List;
-
-import com.example.ecom.dto.ProductDTO;
-
-public interface ProductService {
-	List<ProductDTO> getAllProducts();
-}
